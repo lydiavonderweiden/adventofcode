64e17741281b562be8363f64cadacfdb1be36c2c
lydiavonderweiden <lydia.weiden@tu-dortmund.de>
2023-06-04 16:35:12 +0200

    pieces of part 2 of day 8

--- 21/momo_d8/decoding.py
+++ 21/momo_d8/decoding.py
@@ -1,5 +1,5 @@
 def load():
-    input = open("momo_d8/input")
+    input = open("input")
     values = [line.strip("\n").split(" | ") for line in input.readlines()]
     
     signal = []
@@ -19,21 +19,53 @@ def split_output(output):
             out.append(s)
     return out
 
-def decode_signal(signal):
+def split_lines(input):
+    inp = [i.split() for i in input]
+    return inp
+
+def decode_signal(sig):
+        #print(signal)
+
+    decode = {}
+    twothreefive = []
+    zerosixnine = []
+        
+    for s in sig:
+            
+        match len(s):
+            case 2: decode[1] = s
+            case 4: decode[4] = s
+            case 3: decode[7] = s
+            case 7: decode[8] = s
+            case 5: twothreefive.append(s)
+            case 6: zerosixnine.append(s)
+
+    decode = decode_zerosixnine(zerosixnine, decode)    
+
+    print(decode)
+    return decode
+                
+def decode_zerosixnine(zerosixnine, decode):
+    four = decode[4]
+    one = decode[1]
+
+    for num in zerosixnine:
+        test = [f in num for f in four]
+        if all(test):
+            decode[9] = num
+        elif not all([f in num for f in one]):
+            decode[6] = num
+        else: decode[0] = num
+          
+    return decode
+            
+def decode_twothreefive(twothreefive, decode):
+    one = decode[1]
+    four = decode[4]
     
-    signal = [s.split() for s in signal]
 
-    print(signal)
-
-    for str in signal:
-        match len(str):
-            case 2: eins = str
-            case 4: vier = str
-            case 3: sieben = str
-            case 7: acht = str
-
-
-
+    return decode
+    
 def count_numbers(out):
     counts = 0
 
@@ -48,4 +80,9 @@ if __name__ == "__main__":
     signal, output = load()
     out = split_output(output)
     counts = count_numbers(out)
-    decode_signal(signal)
+    
+    sig = split_lines(signal)
+    out = split_lines(output)
+
+    for s, o in zip(sig, out):
+        decode = decode_signal(s)
