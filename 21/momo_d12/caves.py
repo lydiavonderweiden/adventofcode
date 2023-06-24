2d7fc6dd879516a0df38589b0f6e9c6783108cc8
lydiavonderweiden <lydia.weiden@tu-dortmund.de>
2023-06-24 20:04:39 +0200

    saolved part two of day 12

--- 21/momo_d12/caves.py
+++ 21/momo_d12/caves.py
@@ -2,18 +2,34 @@ from typing import Iterable
 import networkx as nx
 import numpy as np
 
-def find_path(pos, G):
-    assert isinstance(pos, str)
-
+def find_path(pos, G, return_visit):
     if pos == 'end': return ['end']
     
     adj = list(G.neighbors(pos))
-    H = G.copy()
-    if pos.islower(): H.remove_node(pos)
-    
     if len(adj) == 0: return ["DEAD END"]
+    
+    H = G.copy()
+    if pos == 'start':
+        H.remove_node(pos)
+    else:
+        if pos.islower() and return_visit and H.nodes[pos]['visit'] > 0:
+            return ['DEAD END']
 
-    path_tree = [[pos] + find_path(a, H) for a in adj]
+        H.nodes[pos]['visit'] += 1
+
+        if pos.islower() and H.nodes[pos]['visit'] > 1:
+            return_visit = True
+
+        for a in adj:
+            if a.islower() and return_visit and H.nodes[a]['visit'] > 0:  
+                H.remove_node(a) 
+                adj.remove(a)
+                
+        if len(adj) == 0: return ["DEAD END"]
+
+        if pos.islower() and return_visit: H.remove_node(pos)
+
+    path_tree = [[pos] + find_path(a, H, return_visit) for a in adj]
     return path_tree 
 
 def flatten(tree):
@@ -28,7 +44,8 @@ if __name__ == "__main__":
 
     G = nx.Graph()
     G.add_edges_from(edges)
-    liste = find_path('start', G)
-    print(liste)
+    nx.set_node_attributes(G, 0, "visit") 
+    liste = find_path('start', G, False)
+    #print(liste)
     ends_reached = len([1 for e in flatten(liste) if e == "end"])
     print(ends_reached)
