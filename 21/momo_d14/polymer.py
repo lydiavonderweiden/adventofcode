79580645bd470d9525aea80383a1659645b642f8
lydiavonderweiden <lydia.weiden@tu-dortmund.de>
2023-07-09 09:37:24 +0200

    tried to optimize day 14

--- 21/momo_d14/polymer.py
+++ 21/momo_d14/polymer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def split_blocks(polymer):
     blocks =[ "".join([a,b])for a, b in zip(polymer, polymer[1:])]
     return blocks
@@ -16,24 +18,44 @@ def compare(b):
             b = b[0] + rule[1] +b[1]
             break
 
-def pair_insertion(polymer, rules):
-    blocks = split_blocks(polymer)
-
+def pair_insertion(blocks, rules):
+   
     for i, b in enumerate(blocks):
         for rule in rules:
             if b == rule[0]:
                 blocks[i] = b[0] + rule[1] +b[1]
-                break
-    
-    polymer = join_blocks(blocks)
+                break    
 
-    return polymer
+    return blocks
+
+def rearrange_blocks(liste):
+    new_list = []
+
+    for b in liste:
+        new_list.append(liste[0:1])
+        new_list.append(liste[1:2])
+
+    return new_list
+
+def count_elements(polymer):
+    polymer = [e for e in polymer]
+    elements, count = np.unique(polymer, return_counts=True)
+    
+    diff = count.max()-count.min()
+    print(diff)
 
 if __name__ == "__main__":
-    with open("test") as file:
+    with open("input") as file:
         polymer = file.readline().strip()
         rules = [l.strip().split(" -> ") for l in file.readlines()]
 
+    blocks = split_blocks(polymer)
+    for i in range(40):
+        polymer = pair_insertion(blocks, rules)
+        blocks = rearrange_blocks(blocks)
+        print(i)
     
-    polymer = pair_insertion(polymer, rules)
-    print(polymer)
+    polymer = join_blocks(blocks)
+    count_elements(polymer)
+    #print(polymer)
+    #print(len(polymer))
